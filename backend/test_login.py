91df5b60f30de907156d210f9b11ad2be056ca79
Aryan7391 <aryanchand7391@gmail.com>
2026-06-01 09:51:24 +0530

    chore: remove sensitive test scripts

--- backend/test_login.py
+++
@@ -1,4 +0,0 @@
-import requests
-res = requests.post("https://smartroute-production.up.railway.app/api/v1/auth/login", json={"phone": "9999999999", "password": "test1234"})
-print(res.status_code)
-print(res.text)
